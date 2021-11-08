998be9f8fe1d08b3d7c201f87dbe74590fdda76d
LOCO185 <jloco151@gmail.com>
2021-11-08 21:59:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let  score;
+let  letter;
+
+if (score >= 90) {
+    letter = 'A';
+}
+else if (score >= 80){
+    letter = 'B';
+}
+else if (score >= 70){
+    letter = 'C';
+}
+else if (score >= 65){
+    letter = 'D';
+}
+else {
+    letter = 'F';
+}
+
